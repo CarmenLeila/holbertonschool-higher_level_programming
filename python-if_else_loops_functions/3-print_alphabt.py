596267801258452142d993ac519df634af50abae
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-01-25 13:01:55 +0100

    3.fixing bug

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for index in range(97, 123):
-    if index != 101 and index != 113:
-        print("{}".format(chr(index)), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
+        
