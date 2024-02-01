0536d9e0e855eef1e7f8f2ac36bfe091e316c282
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-02-01 10:05:14 +0100

    fixing a bug

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-str = ""
+
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
+    for _list in matrix:
+        i = 0
+
+        for num in _list:
+            print("{:d}".format(num), end="")
+            if i != len(_list) - 1:
+                print("", end=" ")
+            i += 1
         print()
