de8cf9a88e3dd962ad1cedc2a7660f663b193145
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-02-08 09:42:27 +0100

    fixing bug

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+This module is Empty, it does nothing
+"""
+
 
 class Square:
-    """Square class."""
+    """
+    Note:
+        Nothing to do
+
+    Args:
+        no arguments
+
+    Attributes:
+        No Attributes
+    """
     pass
+    
