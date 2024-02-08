6497da05beb8180aac99b4e46fd8b1365f595a0a
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-02-08 10:09:23 +0100

    fixing bugs

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,19 +1,99 @@
 #!/usr/bin/python3
-"""This module creates a class Square."""
+"""
+This module define a class called Square
+Attributes:
+    size (int): it represente the size of a side of
+    the square object
+
+Todo:
+    define class Square
+    define a private instance attribute calle "size"
+    raise TypeError if size isn't integer
+    raise ValueError if size is less that zero
+    Create a public method called area(self)
+    Create a public getter of size attribute
+    Greate a public setter of size attribute
+"""
+
+
 class Square:
+    """
+    Note:
+        This class represente a Square objects
+
+    Attributes:
+        size (int): private instance attribute
+
+    Methods:
+        area(self): a public instance method
+        size(self): a public instance method
+        size(self, size): a public instance method
+
+    Args:
+        size (int)
+    """
     def __init__(self, size=0):
-        self.size = size
+        """
+        Note:
+            This methode initialize a square object
+            It raises TypeError if size is not integer
+            It raises ValueError if size < 0
+
+        Args:
+            self (Square): private square instance object
+            size (int): square object one side size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Note:
+            This method compute the area of the Square object instance
+            Return the area of the self object
+
+        Args:
+            self (Square): the Square instance
+
+        Return:
+            The Square of self
+        """
+        return self.__size * self.__size
+
     @property
     def size(self):
+        """
+        Note:
+            This is a getter of size attribute
+
+        Args:
+            self(Square): square object
+
+        Return:
+            self.size
+        """
         return self.__size
+
     @size.setter
-    def size(self, value):
-        """The setter function for the size attribute."""
-        if not isinstance(value, int):
+    def size(self, size):
+        """
+        Note:
+            This is the setter of self.size
+
+        Args:
+            self(Square): Square object
+            size(int): value to set upon self.size
+
+        Return:
+            Nothing
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-    def area(self):
-        """Return the area of the square."""
-        return self.__size ** 2
+        else:
+            self.__size = size
