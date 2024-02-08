75ac06d4add9393993c8e57c996d55ab446d6b1e
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-02-08 09:59:59 +0100

    fixing bug

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,43 @@
 #!/usr/bin/python3
-"""This module creates a class Square"""
+"""
+This module define a class called Square
+Attributes:
+    size (int): it represente the size of a side of
+    the square object
+
+Todo:
+    define class Square
+    define a private instance attribute calle "size"
+    raise TypeError if size isn't integer
+    raise ValueError if size is less that zero
+"""
 
 class Square:
+     """
+    Note:
+        This class represente a Square objects
+
+    Attributes:
+        size (int) : private instance attribute
+
+    Args:
+        size (int)
+    """
     
         def __init__(self, size=0):
+             """
+        Note:
+            This methode initialize a square object
+            It raises TypeError if size is not integer
+            It raises ValueError if size < 0
+
+        Args:
+            self (Square): private square instance object
+            size (int): square object one side size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
-            if type(size) != int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
