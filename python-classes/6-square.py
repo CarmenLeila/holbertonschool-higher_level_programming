668e2cf4d4e051c7fc00fccedf3235ae80b26e33
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-02-10 21:21:42 +0100

    changing something in code

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,6 +14,9 @@ Todo:
     Create a public getter of size attribute
     Greate a public setter of size attribute
     Create a public instance method my_print(self)
+    Create a private instance attribute __position
+    Create a public instance setter method position(self, pos)
+    Create a public instance getter method position(self)
 """
 
 
@@ -24,33 +27,36 @@ class Square:
 
     Attributes:
         size (int): private instance attribute
+        position(int, int): private instance attribute
 
     Methods:
         area(self): a public instance method
         size(self): a public instance method
         size(self, size): a public instance method
+        position(self): a public instance method
+        position(self, position): a public instance method
         my_print(self): public instance method
 
     Args:
+        self(Square)
         size (int)
+        position(int, int)
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Note:
             This methode initialize a square object
             It raises TypeError if size is not integer
             It raises ValueError if size < 0
+            It initialize the __position attribute
 
         Args:
             self (Square): private square instance object
             size (int): square object one side size
+            position(int, int): 2 elements tuple
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
+        self.position = position
 
     def area(self):
         """
@@ -99,7 +105,43 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-            
+
+    @property
+    def position(self):
+        """
+        Note:
+            this is a position getter function
+
+        Args:
+            self(Square): an object of type square
+
+        Return:
+            (int, int): a two integer tuple represanting position
+         """
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        """
+        Note:
+            This is a position setter function
+
+        Args:
+            self(square): an object of type square
+            position(int, int): 2D position
+
+        Return:
+            Nothing
+        """
+        if position is not None and isinstance(position, tuple) and\
+                len(position) == 2 and\
+                isinstance(position[0], int) and\
+                isinstance(position[1], int) and\
+                position[0] >= 0 and position[1] >= 0:
+            self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def my_print(self):
         """
         Note:
@@ -113,10 +155,10 @@ class Square:
         """
         hashtag = "#"
 
-        if self.__size == 0:
+        if self.size == 0:
             print()
         else:
             print("\n" * self.position[1], end="")
-            for index in range(self.__size):
+            for index in range(0, self.size):
                 print(" " * self.position[0], end="")
-                print(hashtag * self.__size)
+                print(hashtag * self.size)
