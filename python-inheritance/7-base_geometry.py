6fd6031bc334f43e16ed7151a58c2f336fd05cc0
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-02-20 14:04:21 +0100

    fixing bugs

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""defines a class BaseGeometry"""
+"""this is a module"""
 
 
 class BaseGeometry:
-    """class is empty"""
-    pass
+    """this is a new class"""
 
     def area(self):
-        """instance method to calculate the area"""
-        raise Exception('area() is not implemented')
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """instance method for verify if integer is valid"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        
