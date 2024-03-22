7f3814a38aeb0387c0b67e063d3c44a4ca6b2cd7
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-03-22 23:11:46 +0100

    fix bugs

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,15 +9,24 @@ import sys
 
 if __name__ == "__main__":
     """including the guard clause"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    """creating a SQLAlchemy engine that will connect to the MySQL database"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Create a session to interact with the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username,
+        password,
+        database
+    ), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     query = session.query(State).order_by(State.id).all()
+
     session.close()
+
     for state in query:
-        print(f"{states.id}: {states.name}")
+        print("{}: {}".format(state.id, state.name))
