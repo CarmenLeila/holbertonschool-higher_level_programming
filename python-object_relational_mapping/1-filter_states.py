af17b40eee27ab5a64712ee56fb8c7460afeebcf
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-03-22 21:55:25 +0100

    1. correct some bugs

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         port=3306
     )
 
-    cursor = database.cursor()
+    cursor = myconnect.cursor()
     cursor.execute(
         "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     )
