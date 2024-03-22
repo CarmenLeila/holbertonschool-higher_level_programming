ccdb57de68e7452974649f39ef24db826e32cef4
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-03-22 23:23:16 +0100

    First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,30 +3,22 @@
 
 from model_state import Base, State
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    """including the guard clause"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    """creating a SQLAlchemy engine that will connect to the MySQL database"""
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username,
-        password,
-        database
-    ), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).first()
-
-    session.close()
-
+    state = session.query(State).first()
     if state:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
     else:
         print("Nothing")
